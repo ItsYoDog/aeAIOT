20814735d937876dab91ea9435193d9fcbbb5f53
ItsYoDog <107772954+ItsYoDog@users.noreply.github.com>
2023-03-01 15:28:54 +0100

    Startup and Disclaimer

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    cout << "\033[1;32mHello World!\033[0m" << endl;
+int main()
+{
+    std::cout << R"(
+ █████╗ ███████╗███████╗     █████╗ ██╗ ██████╗ ████████╗
+██╔══██╗██╔════╝██╔════╝    ██╔══██╗██║██╔═══██╗╚══██╔══╝
+███████║█████╗  ███████╗    ███████║██║██║   ██║   ██║   
+██╔══██║██╔══╝  ╚════██║    ██╔══██║██║██║   ██║   ██║   
+██║  ██║███████╗███████║    ██║  ██║██║╚██████╔╝   ██║   
+╚═╝  ╚═╝╚══════╝╚══════╝    ╚═╝  ╚═╝╚═╝ ╚═════╝    ╚═╝                                                          
+                                                      )" << std::endl;
+    
+    std::cout << "\033[1;31m" << std::endl << "DISCLAIMER: This software is provided as is, and the author is not responsible for any damage caused by its use." << std::endl << "\033[0m" << std::endl;
+
+    std::string input;
+    std::cout << "Do you accept the disclaimer? (yes/no): ";
+    std::cin >> input;
+
+    if (input == "yes") {
+        std::cout << std::endl << "Welcome to ae's AIOT!" << std::endl;
+    } else {
+        std::cout << std::endl << "Exiting..." << std::endl;
+    }
+
     return 0;
 }

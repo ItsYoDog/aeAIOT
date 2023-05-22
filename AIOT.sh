ac6c1e287add4caae3bc4cb2b7298b09b361097e
ItsYoDog <gercovantfoort2006@gmail.com>
2023-05-22 22:55:40 +0200

    Fix cleanup.sh (and AIOT.sh) so it doesn't delete iself

--- AIOT.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Compile the main.cpp file
-g++ -o main main.cpp
-
-# Run the compiled executable
-./main
--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Delete all executables in the current folder
-find . -maxdepth 1 -type f -executable -delete
-
-# Delete the .vscode folder and its contents
-rm -rf .vscode
-
-echo "Cleanup completed."
